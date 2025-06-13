789515991bc1ca7d3ffca8b2810dff466edd8cfc
homfunctor <thehundredvisions@proton.me>
2025-06-13 03:18:52 -0500

    .

--- modules/nixos/dewm/de-cosmic.nix
+++ modules/nixos/dewm/de-cosmic.nix
@@ -1,3 +1,16 @@
-# todo: system-side cosmic settings
 {
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkIf;
+
+  cfg = config.nixos.opts.dewm.de-cosmic;
+in {
+  config = mkIf cfg.enable {
+    services.desktopManager.cosmic = {
+      enable = true;
+      xwayland.enable = true;
+    };
+  };
 }
