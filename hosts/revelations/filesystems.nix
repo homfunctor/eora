5812493be9eef596a43f42bba52c159cff476963
homfunctor <thehundredvisions@proton.me>
2025-06-12 23:33:32 -0500

    .

--- hosts/revelations/filesystems.nix
+++
@@ -1,7 +0,0 @@
-# CAUTION: fuck this up and you'll need to defile /nix/store
-{inputs, ...}: {
-  # not really secret i just like privacy
-  imports = [
-    inputs.sapadal.modules.nixos.fsys-revelations
-  ];
-}
