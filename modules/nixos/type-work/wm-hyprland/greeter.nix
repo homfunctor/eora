818cbe81bbe2f8771b9552edbd5184c9062ec349
homfunctor <thehundredvisions@proton.me>
2025-06-29 01:12:01 -0500

    ok

--- modules/nixos/type-work/wm-hyprland/greeter.nix
+++ modules/nixos/type-work/wm-hyprland/greeter.nix
@@ -1,1 +1,30 @@
-{}
+# todo: check how stylix applies here
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) getExe;
+in {
+  programs.regreet = {
+    enable = true;
+  };
+
+  services = {
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${getExe pkgs.regreet}";
+          user = "greeter";
+        };
+      };
+    };
+
+    uwsm = {
+      enable = true;
+      session = "${getExe pkgs.hyprland}";
+    };
+  };
+}
