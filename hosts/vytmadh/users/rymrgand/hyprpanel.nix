4955fb7e6e12a6763653b8ad64cdba75d2e21f83
homfunctor <thehundredvisions@proton.me>
2025-06-29 20:18:58 -0500

    ok

--- hosts/vytmadh/users/rymrgand/hyprpanel.nix
+++ hosts/vytmadh/users/rymrgand/hyprpanel.nix
@@ -1,3 +1,4 @@
+# todo: change dashboard settings
 {flake, ...}: let
   inherit (flake.lib) uApp;
 in {
@@ -5,15 +6,15 @@ in {
     hplLayout = {
       "0" = {
         "left" = [
-          "power"
+          "dashboard"
           "workspaces"
           "windowtitle"
         ];
         "middle" = [
           "cpu"
           "ram"
-          "storage"
           "clock"
+          "battery"
         ];
         "right" = [
           "volume"
@@ -33,6 +34,80 @@ in {
       popover = 60;
       power = 75;
       volume = 75;
+    };
+
+    hplLeftcuts = {
+      shortcut1 = {
+        command = uApp "vivaldi";
+        icon = "";
+        tooltip = "Vivaldi";
+      };
+
+      shortcut2 = {
+        command = uApp "steam";
+        icon = "󰓓";
+        tooltip = "Steam";
+      };
+
+      shortcut3 = {
+        command = uApp "alacritty";
+        icon = "";
+        tooltip = "Alacritty";
+      };
+
+      shortcut4 = {
+        command = uApp "alacritty --working-directory eora -e yazi";
+        icon = "󰼪";
+        tooltip = "Browse Flake";
+      };
+    };
+
+    hplRightcuts = {
+      shortcut1 = {
+        command = uApp "librewolf";
+        icon = "󰈹";
+        tooltip = "Librewolf";
+      };
+
+      shortcut3 = {
+        command = uApp "strawberry";
+        icon = "󰝚";
+        tooltip = "Strawberry";
+      };
+    };
+
+    hplDirleft = {
+      directory1 = {
+        command = uApp "nautilus -w";
+        label = "󰋜 Home";
+      };
+
+      directory2 = {
+        command = uApp "nautilus -w Nix";
+        label = " Nix";
+      };
+
+      directory3 = {
+        command = uApp "nautilus -w Rust";
+        label = "󱘗 Rust";
+      };
+    };
+
+    hplDirright = {
+      directory1 = {
+        command = uApp "nautilus -w Books";
+        label = "󱉟 Books";
+      };
+
+      directory2 = {
+        command = uApp "nautilus -w Math";
+        label = "󰿈 Math";
+      };
+
+      directory3 = {
+        command = uApp "nautilus -w Downloads";
+        label = "󰇚 Downloads";
+      };
     };
   };
 
