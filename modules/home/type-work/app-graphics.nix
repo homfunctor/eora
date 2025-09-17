0de18e417d4eb504ca7adaacdc1c5ca367fb6572
homfunctor <thehundredvisions@proton.me>
2025-09-16 22:36:46 -0500

    dds opened by gimp

--- modules/home/type-work/app-graphics.nix
+++ modules/home/type-work/app-graphics.nix
@@ -5,4 +5,8 @@
     kdePackages.kolourpaint
     xournalpp
   ];
+  xdg.mimeApps = {
+    associations.added."image/x-dds" = "gimp.desktop";
+    defaultApplications."image/x-dds" = "gimp.desktop";
+  };
 }
