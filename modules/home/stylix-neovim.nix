4c46031ea36c6142ef21ba14dbbf2bc60ec10827
homfunctor <thehundredvisions@proton.me>
2025-09-25 20:15:29 -0500

    ok

--- modules/home/stylix-neovim.nix
+++ modules/home/stylix-neovim.nix
@@ -1,7 +1,7 @@
 {
   stylix.targets.nixvim = {
     enable = true;
-    # plugin = "base16-nvim";
+    plugin = "base16-nvim";
     transparentBackground = {
       main = true;
       signColumn = true;
