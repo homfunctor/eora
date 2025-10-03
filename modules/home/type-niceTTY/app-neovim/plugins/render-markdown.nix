92f7638a8b250e186b4b7ee29e92e7277028516e
homfunctor <thehundredvisions@proton.me>
2025-10-03 00:49:15 -0500

    markdown, overriding new hyprpanel stylix options because i don't want to deal with it right now

--- modules/home/type-niceTTY/app-neovim/plugins/render-markdown.nix
+++ modules/home/type-niceTTY/app-neovim/plugins/render-markdown.nix
@@ -94,11 +94,11 @@ in {
       extraConfigLua = with colors; ''
         vim.api.nvim_set_hl(0, 'RenderMarkdownBorder', { link = 'Normal' })
 
-        vim.api.nvim_set_hl(0, 'RenderMarkdownHeading1', { bold = true, foreground = ${base0E} })
-        vim.api.nvim_set_hl(0, 'RenderMarkdownHeading2', { bold = true, foreground = ${base0A} })
-        vim.api.nvim_set_hl(0, 'RenderMarkdownHeading3', { bold = true, foreground = ${base08} })
-        vim.api.nvim_set_hl(0, 'RenderMarkdownCode', { background = ${base02}, foreground = ${base07} })
-        vim.api.nvim_set_hl(0, 'RenderMarkdownBlockquote', { italic = true, foreground = ${base03}})
+        vim.api.nvim_set_hl(0, 'RenderMarkdownHeading1', { bold = true, foreground = '${base0E}' })
+        vim.api.nvim_set_hl(0, 'RenderMarkdownHeading2', { bold = true, foreground = '${base0A}' })
+        vim.api.nvim_set_hl(0, 'RenderMarkdownHeading3', { bold = true, foreground = '${base08}' })
+        vim.api.nvim_set_hl(0, 'RenderMarkdownCode', { background = '${base02}', foreground = '${base07}' })
+        vim.api.nvim_set_hl(0, 'RenderMarkdownBlockquote', { italic = true, foreground = '${base03}'})
 
         -- Force non-rounded borders in the render window
         vim.api.nvim_create_autocmd('FileType', {
