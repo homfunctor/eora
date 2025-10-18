1beb0542e84c90d81b5aac4750762e20c864d68b
homfunctor <thehundredvisions@proton.me>
2025-10-18 04:52:44 -0500

    attempting to de-round qt. minimal success.

--- modules/home/type-work/opt-stylix/qt.nix
+++ modules/home/type-work/opt-stylix/qt.nix
@@ -1,4 +1,28 @@
 # https://github.com/nix-community/stylix/blob/master/modules/qt/hm.nix
+# Added the following to kvconfig.mustache:
+# [General]
+# composite=false
+# rounded_menu=false
+# menu_combo_button=false
+# rounded_dialogs=false
+#
+# [Layout]
+# layout_spacing=2
+# layout_margin=2
+# submenu_overlap=0
+#
+# [Metrics]
+# radius=0
+#
+# [Compositing]
+# shadow_size=0
+# popup_menu_blurring=false
+#
+# [Hacks]
+# transient_scrollbar=true
+# inline_toolbar_icon=false
+# no_window_pattern=true
+# does it work? no idea.
 {
   config,
   inputs,
