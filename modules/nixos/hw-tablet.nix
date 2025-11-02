612592f012601c353afc3e418a93a7e361a7afb7
homfunctor <thehundredvisions@proton.me>
2025-11-02 01:34:15 -0500

    ok

--- modules/nixos/hw-tablet.nix
+++ modules/nixos/hw-tablet.nix
@@ -1,17 +1,18 @@
 {pkgs, ...}: {
   # daemon launched when hyprland starts
-  # Button 1: Control+Shift+R
-  # Button 2: Control+Shift+P
-  # Button 3: Control+Shift+O
-  # Button 4: Control+Z (Undo)
-  # Button 5: Left arrow key
-  # Button 6: Down arrow key
-  # Button 7: Up arrow key
-  # Button 8: Right arrow key
+  # Button 1: Control+D7 (toggle lines)
+  # Button 2: Control+Shift+P (write)
+  # Button 3: Control+Z (Undo)
+  # Button 4: Control+Y (Redo)
+  # Button 5: Down arrow key
+  # Button 6: Up arrow key
+  # Button 7: Control+Shift+O (select object)
+  # Button 8: Control+Shift+R (select region)
   # Button 9: Control+Shift+Application+C (calculator)
   # Button 10: Control+Shift+Application+W (yazi work directory)
-  # Button 11:
-  # Button 12:
+  # do NOT confuse these two
+  # Button 11: Kill
+  # Button 12: Fullscreen
   hardware.opentabletdriver.enable = true;
   environment.systemPackages = with pkgs; [
     evtest
