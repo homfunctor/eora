8872065b34069dd5d81ae174c68989d2ac65c031
homfunctor <thehundredvisions@proton.me>
2025-11-03 13:53:34 -0600

    ok

--- modules/nixos/hw-tablet.nix
+++ modules/nixos/hw-tablet.nix
@@ -6,7 +6,7 @@
   # Button 4: Control+Y (Redo)
   # Button 5: Down arrow key
   # Button 6: Up arrow key
-  # Button 7: Control+Shift+O (select object)
+  # Button 7: Delete
   # Button 8: Control+Shift+R (select region)
   # Button 9: Control+Shift+Application+C (calculator)
   # Button 10: Control+Shift+Application+W (yazi work directory)
