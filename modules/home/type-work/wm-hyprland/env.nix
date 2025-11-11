c3968a1e29c5861eb30204b1f148e7547202a655
homfunctor <thehundredvisions@proton.me>
2025-11-10 20:05:38 -0600

    ok

--- modules/home/type-work/wm-hyprland/env.nix
+++ modules/home/type-work/wm-hyprland/env.nix
@@ -1,7 +1,9 @@
-# todo
 # already set:
-#   XDG_RUNTIME_DIR = "/run/user/$UID";
-#   DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$UID/bus";
+# XDG_RUNTIME_DIR = "/run/user/$UID";
+# DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$UID/bus";
+# XDG_CURRENT_DESKTOP = "Hyprland";
+# XDG_SESSION_TYPE = "wayland";
+# _JAVA_AWT_WM_NONREPARENTING = "1";
 {config, ...}: {
   xdg.configFile = {
     "electron-flags.conf".text = ''
@@ -10,44 +12,38 @@
       --ozone-platform=wayland
     '';
 
-    # "uwsm/env".text = ''
-    #   export BROWSER=${apps.browser.exe}
-    #   export CLUTTER_BACKEND=wayland
-    #   export DIRENV_LOG_FORMAT=1
-    #   export DISABLE_QT5_COMPAT=0
-    #   export DISABLE_QT_COMPAT=0
-    #   export EDITOR=${apps.text.exe}
-    #   export ELECTRON_OZONE_PLATFORM_HINT="wayland";
-    #   export GDK_BACKEND=wayland,x11
-    #   export GDK_SCALE=1
-    #   export GNOME_KEYRING_CONTROL="$XDG_RUNTIME_DIR/keyring"
-    #   export HYPRCURSOR_SIZE=${toString config.stylix.cursor.size}
-    #   export HYPRCURSOR_THEME=${config.stylix.cursor.name}
-    #   export HYPRLAND_NO_RT=1
-    #   export MOZ_DISABLE_RDD_SANDBOW=1
-    #   export MOZ_ENABLE_WAYLAND=1
-    #   export NIXOS_OZONE_WL=1
-    #   export QT_AUTO_SCREEN_SCALE_FACTOR=1
-    #   export QT_IM_MODULE=wayland
-    #   export QT_QPA_PLATFORM=wayland;xcb
-    #   export QT_QPA_PLATFORMTHEME=qt5ct
-    #   export QT_SCALE_FACTOR=1
-    #   export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-    #   export SDL_VIDEODRIVER=wayland,x11
-    #   export SHELL=${apps.shell.exe}
-    #   export TERMINAL=${apps.terminal.exe}
-    #   export VISUAL=${apps.text.exe}
-    #   export WLR_BACKEND=${osConfig.nixos.opts.wlrBR}
-    #   export WLR_RENDERER=${osConfig.nixos.opts.wlrBR}
-    #   export XDG_CURRENT_DESKTOP=Hyprland
-    #   export XDG_SESSION_DESKTOP=Hyprland
-    #   export XDG_SESSION_TYPE=wayland
-    #   export _JAVA_AWT_WM_NONREPARENTING=1
-    # '';
-    #
-    # "uwsm/env-hyprland".text = ''
-    #   export XDG_CURRENT_DESKTOP=Hyprland
-    #   export XDG_SESSION_DESKTOP=Hyprland
-    # '';
+    home.sessionVariables = with config.home.opts; {
+      BROWSER = apps.browser.exe;
+      CLUTTER_BACKEND = "wayland";
+      DIRENV_LOG_FORMAT = "1";
+      DISABLE_QT5_COMPAT = "0";
+      DISABLE_QT_COMPAT = "0";
+      EDITOR = apps.text.exe;
+      ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+      GDK_BACKEND = "wayland";
+      GDK_SCALE = "1";
+      GNOME_KEYRING_CONTROL = "$XDG_RUNTIME_DIR/keyring";
+      HYPRCURSOR_SIZE = toString config.stylix.cursor.size;
+      HYPRCURSOR_THEME = config.stylix.cursor.name;
+      HYPRLAND_NO_RT = "1";
+      MOZ_DBUS_REMOTE = 1;
+      MOZ_DISABLE_RDD_SANDBOW = "1";
+      MOZ_ENABLE_WAYLAND = 1;
+      MOZ_WAYLAND_USE_VAAPI = 1;
+      NIXOS_OZONE_WL = "1";
+      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      QT_IM_MODULE = "wayland";
+      QT_QPA_PLATFORM = "wayland;xcb";
+      QT_QPA_PLATFORMTHEME = "qt5ct";
+      QT_SCALE_FACTOR = "1";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      SDL_VIDEODRIVER = "wayland,x11";
+      SHELL = apps.shell.exe;
+      TERMINAL = apps.terminal.exe;
+      VISUAL = apps.text.exe;
+      WLR_BACKEND = osConfig.nixos.opts.wlrBR;
+      WLR_RENDERER = osConfig.nixos.opts.wlrBR;
+      XDG_SESSION_DESKTOP = "Hyprland";
+    };
   };
 }
