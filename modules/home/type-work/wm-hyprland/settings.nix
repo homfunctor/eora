c35fc6e47295388d6b4e8d43288c6a408c8edb22
homfunctor <thehundredvisions@proton.me>
2025-11-12 02:01:32 -0600

    hyprshell issues and permissions not being stored?

--- modules/home/type-work/wm-hyprland/settings.nix
+++ modules/home/type-work/wm-hyprland/settings.nix
@@ -32,19 +32,15 @@ in {
         };
       };
 
-      # dwindle = {
-      #   force_split = 0;
-      #   preserve_split = true;
-      #   pseudotile = true;
-      #   smart_resizing = true;
-      #   smart_split = true;
-      #   special_scale_factor = 1.0;
-      #   split_width_multiplier = 1.0;
-      #   use_active_for_splits = true;
-      # };
-
-      master = {
-        # todo
+      dwindle = {
+        force_split = 0;
+        preserve_split = true;
+        pseudotile = true;
+        smart_resizing = true;
+        smart_split = true;
+        special_scale_factor = 1.0;
+        split_width_multiplier = 1.0;
+        use_active_for_splits = true;
       };
 
       ecosystem.no_donation_nag = true;
@@ -55,7 +51,7 @@ in {
         border_size = mkDefault 5;
         gaps_in = mkDefault 5;
         gaps_out = mkDefault 0;
-        layout = mkDefault "master";
+        layout = mkDefault "dwindle";
         resize_on_border = true;
       };
 
