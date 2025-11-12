cbc6f0d5825c54fa57376ed7ca2eb68183e5d7d8
homfunctor <thehundredvisions@proton.me>
2025-11-12 02:34:39 -0600

    ok

--- modules/nixos/chaotic-kernel.nix
+++ modules/nixos/chaotic-kernel.nix
@@ -2,7 +2,7 @@
   boot.kernelPackages = pkgs.linuxPackages_cachyos;
   services.scx = {
     enable = true;
-    package = pkgs.scx_git.full;
+    package = pkgs.scx.full;
     scheduler = "scx_lavd";
     extraArgs = ["--performance"];
   };
