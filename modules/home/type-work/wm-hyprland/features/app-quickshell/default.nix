e526338e4bdbc9ee0cd469fe412a25369c3e5e16
homfunctor <thehundredvisions@proton.me>
2025-11-25 14:45:24 -0600

    ok

--- modules/home/type-work/wm-hyprland/features/app-quickshell/default.nix
+++ modules/home/type-work/wm-hyprland/features/app-quickshell/default.nix
@@ -5,12 +5,12 @@
   pkgs,
   ...
 }: {
-  # check if needed
-  # sessionVariables.QML2_IMPORT_PATH = lib.concatStringsSep ":" [
-  #   "${perSystem.quickshell.default}/lib/qt-6/qml"
-  #   "${pkgs.kdePackages.qtdeclarative}/lib/qt-6/qml"
-  #   "${pkgs.kdePackages.kirigami.unwrapped}/lib/qt-6/qml"
-  # ];
+  home.sessionVariables.QML2_IMPORT_PATH = lib.concatStringsSep ":" [
+    "${osConfig.nixos.opts.hypr.quickshell.pkg}/lib/qt-6/qml"
+    "${pkgs.kdePackages.qtdeclarative}/lib/qt-6/qml"
+    "${pkgs.kdePackages.kirigami.unwrapped}/lib/qt-6/qml"
+  ];
+
   programs.quickshell = {
     enable = true;
     package = osConfig.nixos.opts.hypr.quickshell.pkg;
