0448637aa0264193a4c00b0ba8f1f9180caca83d
homfunctor <thehundredvisions@proton.me>
2025-12-11 12:26:07 -0600

    ok

--- modules/home/wm-niri/shell/default.nix
+++ modules/home/wm-niri/shell/default.nix
@@ -8,6 +8,9 @@
   ...
 }: let
   inherit (config.home.opts) userName;
+  nExe = cmd:
+    [(lib.getExe config.programs.noctalia-shell.package) "ipc" "call"]
+    ++ (lib.splitString "" cmd);
 in {
   imports = [
     ./bar.nix
@@ -17,52 +20,59 @@ in {
     ./stylix.nix
     inputs.noctalia.homeModules.default
   ];
-
-  programs.noctalia-shell = {
-    enable = true;
-    systemd.enable = true;
-
-    settings =
+  programs = {
+    niri.settings.spawn-at-startup = [
       {
-        general = {
-          dock.enabled = false;
-          allowPanelsOnScreenWithoutBar = true;
-          avatarImage = "${inputs.sapadal}/assets/${userName}.png";
-          forceBlackScreenCorners = false;
-          showScreenCorners = false;
-
-          boxRadiusRatio = 0;
-          iRadiusRatio = 0;
-          radiusRatio = 0;
-          scaleRatio = 1;
-          screenRadiusRatio = 0;
-
-          animationDisabled = false;
-          animationSpeed = 1;
-          enableShadows = true;
-          shadowDirection = "bottom_left";
-          shadowOffsetX = 2;
-          shadowOffsetY = 3;
-        };
-
-        audio = {
-          externalMixer = "${lib.getExe pkgs.pwvucontrol}";
-          preferredPlayer = config.home.opts.apps.audio.exe;
-        };
-
-        sessionMenu.showHeader = false;
-
-        ui = {
-          fontDefault = config.stylix.fonts.sansSerif.name;
-          fontDefaultScale = 1.25;
-          fontFixed = config.stylix.fonts.monospace.name;
-          fontFixedScale = 1.25;
-          tooltipsEnabled = true;
-          panelBackgroundOpacity = 1;
-          panelsAttachedToBar = true;
-          settingsPanelAttachToBar = true;
-        };
+        command = nExe "volume muteInput";
       }
-      // osConfig.nixos.opts.niri.misc;
+    ];
+
+    noctalia-shell = {
+      enable = true;
+      systemd.enable = true;
+
+      settings =
+        {
+          general = {
+            dock.enabled = false;
+            allowPanelsOnScreenWithoutBar = true;
+            avatarImage = "${inputs.sapadal}/assets/${userName}.png";
+            forceBlackScreenCorners = false;
+            showScreenCorners = false;
+
+            boxRadiusRatio = 0;
+            iRadiusRatio = 0;
+            radiusRatio = 0;
+            scaleRatio = 1;
+            screenRadiusRatio = 0;
+
+            animationDisabled = false;
+            animationSpeed = 1;
+            enableShadows = true;
+            shadowDirection = "bottom_left";
+            shadowOffsetX = 2;
+            shadowOffsetY = 3;
+          };
+
+          audio = {
+            externalMixer = "${lib.getExe pkgs.pwvucontrol}";
+            preferredPlayer = config.home.opts.apps.audio.exe;
+          };
+
+          sessionMenu.showHeader = false;
+
+          ui = {
+            fontDefault = config.stylix.fonts.sansSerif.name;
+            fontDefaultScale = 1.25;
+            fontFixed = config.stylix.fonts.monospace.name;
+            fontFixedScale = 1.25;
+            tooltipsEnabled = true;
+            panelBackgroundOpacity = 1;
+            panelsAttachedToBar = true;
+            settingsPanelAttachToBar = true;
+          };
+        }
+        // osConfig.nixos.opts.niri.misc;
+    };
   };
 }
