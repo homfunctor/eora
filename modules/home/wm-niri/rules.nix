8289573bd4a3f5ebac8d72cb30f8f8ccb12cd9fd
homfunctor <thehundredvisions@proton.me>
2025-12-11 11:29:05 -0600

    ok

--- modules/home/wm-niri/rules.nix
+++ modules/home/wm-niri/rules.nix
@@ -1,1 +1,48 @@
-{}
+{
+  programs.niri.settings = {
+    window-rules = [
+      {
+        matches = [{is-floating = true;}];
+        shadow.enable = true;
+      }
+      {
+        matches = [{app-id = "blueman-manager";}];
+        open-floating = true;
+      }
+      {
+        matches = [{app-id = "com.saivert.pwvucontrol";}];
+        open-floating = true;
+      }
+      {
+        matches = [{app-id = "nm-connection-editor";}];
+        open-floating = true;
+      }
+      {
+        matches = [{app-id = "org.gnome.Calculator";}];
+        open-floating = true;
+      }
+      {
+        matches = [{app-id = "org.gnome.Calendar";}];
+        open-floating = true;
+      }
+      {
+        matches = [{app-id = "org.gnome.FileRoller";}];
+        open-floating = true;
+      }
+      {
+        matches = [{title = "^Picture-in-Picture$";}];
+        default-column-width.fixed = 500;
+        default-window-height.fixed = 250;
+        open-floating = true;
+      }
+      {
+        matches = [{app-id = "xdg-desktop-portal-gnome";}];
+        open-floating = true;
+      }
+      {
+        matches = [{app-id = "xdg-desktop-portal-gtk";}];
+        open-floating = true;
+      }
+    ];
+  };
+}
