33c391906a45f38480c4fe0e550c51b743a8cdb1
homfunctor <thehundredvisions@proton.me>
2026-01-01 16:24:20 -0600

    ok

--- hosts/revelations/fish.nix
+++ hosts/revelations/fish.nix
@@ -1,9 +1,9 @@
 # extra aliases for Revelations
 {
   nixos.opts.shellAliases = {
-    # sometimes you wanna game on the side
-    "m1" = "xrandr --output DP-1 --primary";
-    "m2" = "xrandr --output HDMI-A-1 --primary";
+    # todo: check if still needed for stellaris
+    # "m1" = "xrandr --output DP-1 --primary";
+    # "m2" = "xrandr --output HDMI-A-1 --primary";
     chwine = "LANG=zh_CN.UTF-8 wine";
     jpwine = "LANG=ja_JP.utf8 wine";
     # useful when making mods
