5572cb806bc58b4abee95e2b0baeded4b6f200da
homfunctor <thehundredvisions@proton.me>
2026-03-11 05:41:45 -0500

    ok

--- modules/nixos/type-personal/opt-gaming.nix
+++ modules/nixos/type-personal/opt-gaming.nix
@@ -1,14 +1,14 @@
 # stellaris
-#   env --unset=SDL_VIDEODRIVER gamemoderun %command%
+#   env --unset=SDL_VIDEODRIVER %command%
 #
 # lotro
 #   launch options:
-#     env --unset=TZ PROTON_ENABLE_WAYLAND=1 gamemoderun %command%
+#     env --unset=TZ PROTON_ENABLE_WAYLAND=1 %command%
 #   timezone unset otherwise it uses UTF-0
 #   fps no longer drops on focus loss. wayland is magick.
 #
 # civ 5 (vox populi)
-#   launch options: env --unset=TZ  PROTON_ENABLE_WAYLAND=1 taskset --cpu-list 0-7 gamemoderun %command%
+#   launch options: env --unset=TZ  PROTON_ENABLE_WAYLAND=1 taskset --cpu-list 0-7 %command%
 #   running with wayland fixes the "feature" of audio muting on focus loss
 {inputs, ...}: {
   imports = with inputs.nix-gaming.nixosModules; [
@@ -17,24 +17,7 @@
     wine
   ];
 
-  # environment.systemPackages = [pkgs.gamemode];
-
   programs = {
-    gamemode = {
-      enable = false;
-      settings = {
-        general = {
-          renice = 15;
-          softrealtime = "auto";
-        };
-
-        # gpu = {
-        #   amd_performance_level = "high";
-        #   apply_gpu_optimisations = "accept-responsibility";
-        # };
-      };
-    };
-
     steam = {
       enable = true;
       platformOptimizations.enable = true;
